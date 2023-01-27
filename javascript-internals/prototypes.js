f04470dad39b96f53582579dee945a054bff19ae
Ricky Medina <rickmedina85@gmail.com>
2023-01-27 18:21:25 -0400

    u

--- javascript-internals/prototypes.js
+++ javascript-internals/prototypes.js
@@ -1,28 +1,39 @@
+/* In serious code use Object.getPrototypeOf instead of __proto__  */
+const getProto = Object.getPrototypeOf
 
+// ** String is a function constructor
+// String.protytype -> obj
+// String.__proto__ -> NativeCode
+// NativeCode.__proto === String.prototype.__proto__
+
+// console.log("a".__proto__ === String.prototype)
+
+// true:
+// "a".__proto__.__proto__ ===
+// String.prototype.__proto__ ===
+// String.__proto__.__proto__
+
+const PP = String.prototype.__proto__
+// const PP = String.__proto__.__proto__
+// console.log(Object.getOwnPropertyNames(PP))
 /*
-String is a function constructor so it has a .protototype
-This ‘prototype’ property points to the object that will be
-asigned as the prototype of instances created with that function
-when using ‘new’ ("" in String's case)
+PP has:
+	[
+	'constructor',
+	'__defineGetter__',
+	'__defineSetter__',
+	'hasOwnProperty',
+	'__lookupGetter__',
+	'__lookupSetter__',
+	'isPrototypeOf',
+	'propertyIsEnumerable',
+	'toString',
+	'valueOf',
+	'__proto__',  ---------------->  null (root prototype)
+	'toLocaleString'
+	]
 */
 
-const proto = Object.getPrototypeOf
-
-const StringConst_P = proto(String)
-// String.protytype -> obj
-// getPrototypeOf(String) -> NativeCode
-
-const strInstance_P = proto("a")
-// true
-// console.log(getPrototypeOf("a") === String.prototype)
-
-const strInstance_PP = proto(strInstance_P)
-const StringConst_PP = proto(StringConst_P)
-// true:
-// console.log(strInstance_PP == StringConst_PP)
-
-const PP = StringConst_PP
-// console.log(Object.getOwnPropertyNames(PP))
-
-// null
-const root_P = proto(PP)
+// can't do:
+// Object.setPrototypeOf(PP, {})
+// bc PP.__proto__ is immutable
