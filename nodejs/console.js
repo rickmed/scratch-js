dbd8bf5c768a31c6c826a732be977eddec468333
Ricky Medina <rickmedina85@gmail.com>
2023-02-01 08:09:07 -0400

    update

--- nodejs/console.js
+++
@@ -1,7 +0,0 @@
-console.log('YOO')
-console.group()
-console.group()
-console.log('HEY')
-console.groupEnd()
-console.group()
-console.log('DONE')
---
+++ nodejs/console/console.js
@@ -0,0 +1,21 @@
+
+const origWrite = process.stdout.write
+
+let logs = []
+
+process.stdout.write = (data) => {
+	logs.push(data)
+	// origWrite.call(process.stdout, "logged: " + data)
+}
+
+console.group()
+console.log("\x1b[31m  HOLA  \x1b[m")
+console.dir("\x1b[31m  HOLA  \x1b[m")
+console.groupEnd()
+// console.group()
+console.dir('DONE')
+
+
+process.stdout.write = origWrite
+
+console.dir(logs)
