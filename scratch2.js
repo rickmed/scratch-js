ee517489ae06306b8ad1b7c133afd0d21012dd92
Ricardo Medina <rickmedina85@gmail.com>
2023-03-24 14:16:16 -0400

    u

--- scratch2.js
+++ scratch2.js
@@ -22,8 +22,27 @@ go(player, "pong", table)
 yield put(table, { hits: 0 });
 
 
-// example of cancellation with timeout
-// need a way
+
+/* example of sophi coordinator
+
+A channel needs to be created so that workers pass messages to it (or
+and actor with a channel attached) -> actor.send(msg)
+
+Sophi stages:
+1) look for testFiles
+	since look for testFiles needs to wait for I/O bc it is recursive, step 2
+	can start in parallel
+
+2) loadTestFiles: import(testFiles) and make testSuites (work stealing by workers)
+	1 and 2 can be ran in parallel (coordinator launches both in parallel)
+
+3) onlyUsed checkpoint:
+	await(onlyUsed: file | filesImported)
+		the coordinator actor must passed to the loadTestFiles workers.
 
 
 
+4) run the tests...
+
+
+*/
--- scratch3.js
+++ scratch3.js
@@ -1,7 +1,66 @@
-async function hi() {
-	return new Promise(res => setTimeout(() => res("hello"), 100))
-}
+import { start } from "ribu"
 
-const epa = hi()
-console.log(epa)
-console.log("yooo")
+// start is important for testing, so modules are pure (one scheduler per system)
+const { spawn } = start()
+
+// me === self()
+spawn(function* coord({me}) {
+
+	// yield to ribu knows this is a child of coord
+	// both below are launched in parallel
+	yield spawn(locateFilePaths, me)
+	yield spawn(loadFiles, pid)
+
+})
+
+
+
+
+/*
+me()
+
+// need to access selfPID() here somehow
+spawn(function* coor({pid}) {
+	coor.pid
+
+})
+
+
+*/
+
+
+/*
+ways to "connect" actors
+
+// 1) weird bc locateFiles should be first in OOO-pipeline
+// but need loadFiles pid so that locateFiles knows where to send data
+
+spawn(function* coord({me}) {
+
+	const pid = yield spawn(loadFiles, me)
+	yield spawn(locateFiles, pid)
+
+})
+
+
+// 2) locateFilesPaths send to that channel, loadFiles receives on same channel
+
+spawn(function* coord({me}) {
+
+	const _ch = ch()
+	yield spawn(locateFilePaths, _ch)
+	yield spawn(loadFiles, _ch, me)
+
+})
+
+
+// 3) implementation???
+
+spawn(function* coord({me}) {
+
+	const locate = yield spawn(locateFilePaths, _ch)
+	const load = yield spawn(loadFiles, _ch, me)
+	locate.pipe(load)
+
+})
+*/
