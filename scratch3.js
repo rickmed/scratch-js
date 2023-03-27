eb77f8e35b9d34a690c4007670abad429164fe64
Ricardo Medina <rickmedina85@gmail.com>
2023-03-27 11:52:16 -0400

    u

--- scratch3.js
+++ scratch3.js
@@ -6,17 +6,51 @@ const { spawn } = start()
 // me === self()
 spawn(function* coord({me}) {
 
-	// yield to ribu knows this is a child of coord
-	// both below are launched in parallel
-	yield spawn(locateFilePaths, me)
-	yield spawn(loadFiles, pid)
+	// yield let ribu knows this is a child of coord
+	// both are launched in parallel
+	const locatePID = yield spawn(locateFilesPath, me)
+	yield spawn(loadFiles, locatePID)
 
 })
 
+async function* locateFilesPath($coord) {
+	const {testFiles: {folders, subString, extensions}} = sophiConfig
+
+	let files = await Promise.all(folders.map(async folder => lookRecursive(folder)))
+	files = files.flat().filter(hasCorrectNamesAndExtensions)
+	return files
+
+	async function lookRecursive(dirPath) {
+		const files = await fs.readdir(dirPath, { withFileTypes: true })
+		const fileNames = await Promise.all(files.map(async file => {
+			const filePath = path.join(dirPath, file.name)
+			if (file.isDirectory()) {
+				return lookRecursive(filePath)
+			} else {
+				return filePath
+			}
+		}))
+		return fileNames.flat()
+	}
+
+	function hasCorrectNamesAndExtensions(file) {
+		const fileExt = path.extname(file).slice(1)
+		if (!extensions.includes(fileExt)) {
+			return false
+		}
+		for (const str of subString) {
+			if (file.includes(str)) {
+				return true
+			}
+		}
+		return false
+	}
+}
 
 
 
-/*
+
+/**** self() api options ***
 me()
 
 // need to access selfPID() here somehow
@@ -29,11 +63,11 @@ spawn(function* coor({pid}) {
 */
 
 
-/*
-ways to "connect" actors
+/**** ways to "connect" actors ***
 
-// 1) weird bc locateFiles should be first in OOO-pipeline
-// but need loadFiles pid so that locateFiles knows where to send data
+1) Elixir style
+	- bad bc locateFiles should be first in OOO-pipeline
+	but need loadFiles pid so that locateFiles knows where to send data
 
 spawn(function* coord({me}) {
 
@@ -55,12 +89,17 @@ spawn(function* coord({me}) {
 
 
 // 3) implementation???
+ie, where should locate send stuff?
 
 spawn(function* coord({me}) {
 
-	const locate = yield spawn(locateFilePaths, _ch)
-	const load = yield spawn(loadFiles, _ch, me)
+	const locate = yield spawn(locateFilePaths)
+	const load = yield spawn(loadFiles, me)
 	locate.pipe(load)
+
+	// or (if you don't need to reference load elsewhere)
+
+	yield spawn(locateFilePaths).pipe(loadFiles, ...args)
 
 })
 */
