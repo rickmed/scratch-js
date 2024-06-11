f9e3492b91f0e96d68edeadf054742cb7b55a83b
Ricardo Medina <medina.r.dev@gmail.com>
2024-06-10 23:14:39 -0400

    feat: add python scratch

---
+++ python/scratch.py
@@ -0,0 +1,15 @@
+# str1 = "dad"
+# str2 = "two"
+# num = 23
+
+# print(str1 + num)
+
+hi = 'epale'
+
+def hello(name):
+   hi = 'epale'
+   def inner():
+      print(hi)
+   inner()
+
+hello('rick')
